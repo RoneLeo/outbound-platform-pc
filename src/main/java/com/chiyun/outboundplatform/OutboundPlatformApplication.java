9a3273144c418ca315e78ccef40eefc78760e4b2
lihuan <964739148@qq.com>
2019-03-11 15:27:00 +0800

    添加数据库配置

--- src/main/java/com/chiyun/outboundplatform/OutboundPlatformApplication.java
+++ src/main/java/com/chiyun/outboundplatform/OutboundPlatformApplication.java
@@ -2,6 +2,12 @@ package com.chiyun.outboundplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 public class OutboundPlatformApplication {
@@ -10,4 +16,27 @@ public class OutboundPlatformApplication {
 		SpringApplication.run(OutboundPlatformApplication.class, args);
 	}
 
+	@Bean
+	public Converter<String, Date> addNewConvert() {
+		return new Converter<String, Date>() {
+			@Override
+			public Date convert(String source) {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+				Date date = null;
+				try {
+					date = sdf.parse((String) source);
+				} catch (ParseException e) {
+				}
+				if (date == null) {
+					try {
+						date = sdf2.parse((String) source);
+					} catch (ParseException e) {
+						return null;
+					}
+				}
+				return date;
+			}
+		};
+	}
 }

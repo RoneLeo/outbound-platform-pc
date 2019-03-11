f9efcb83ec0cb3cc773ef4ad184145a20e020566
wazto123@163.com <WANGwazto123>
2019-03-11 14:55:54 +0800

    追金侠  初始化

--- README.md
+++
@@ -1,3 +0,0 @@
-# outbound-platform
-
-追金侠、外访平台
---
+++ src/main/java/com/chiyun/outboundplatform/OutboundPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.chiyun.outboundplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboundPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutboundPlatformApplication.class, args);
+	}
+
+}

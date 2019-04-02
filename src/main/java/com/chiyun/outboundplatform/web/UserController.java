e0dd145e776f30b9f6695e55c1fa2cda5c2c3aa0
qiu <2411929973@qq.com>
2019-04-02 12:15:30 +0800

    退出登录删掉判断登录

--- src/main/java/com/chiyun/outboundplatform/web/UserController.java
+++ src/main/java/com/chiyun/outboundplatform/web/UserController.java
@@ -224,10 +224,6 @@ public class UserController {
     @ApiOperation(value="修改密码")
     @RequestMapping("/changePassword")
     public ApiResult<Object> changePassword(int id, String mm) throws Exception {
-        //判断是否登录
-        HttpSession session = SessionHelper.getSession();
-        ApiResult<Object> isLogin = SessionUtil.isLogin(session);
-        if (isLogin.getResCode() < 200) return isLogin;
         //查询是否有该用户
         UserEntity oldUserEntity = userReposity.findById(id);
         if (oldUserEntity == null) {

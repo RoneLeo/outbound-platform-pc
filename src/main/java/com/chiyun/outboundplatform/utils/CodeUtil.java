d55b6a80290e6a15e055d49288b8e28a54ab5e2c
qiu <2411929973@qq.com>
2019-04-03 14:28:37 +0800

    修改授权码为纯数字

--- src/main/java/com/chiyun/outboundplatform/utils/CodeUtil.java
+++ src/main/java/com/chiyun/outboundplatform/utils/CodeUtil.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 public class CodeUtil {
     /** 自定义进制（选择你想要的进制数，不能重复且最好不要0、1这些容易混淆的字符） */
-    private static final char[] r=new char[]{'q', 'w', 'e', '8', 's', '2', 'd', 'z', 'x', '9', 'c', '7', 'p', '5', 'k', '3', 'm', 'j', 'u', 'f', 'r', '4', 'v', 'y', 't', 'n', '6', 'b', 'g', 'h'};
+    private static final char[] r=new char[]{'4', '3', '5', '8', '9', '2', '6', '7','1'};
     /** 定义一个字符用来补全邀请码长度（该字符前面是计算出来的邀请码，后面是用来补全用的） */
     private static final char b='a';
     /** 进制长度 */
     private static final int binLen=r.length;
     /** 邀请码长度 */
-    private static final int s=6;
+    private static final int s=4;
     /** 补位字符串 */
-    private static final String e="atgsghj";
+    private static final String e="234567";
 
     /**
      * 根据ID生成随机码

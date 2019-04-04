7780dc1254be842c649d25fb2cfa9d1037c54775
lihuan <964739148@qq.com>
2019-04-04 10:23:52 +0800

    修改查询任务信息及案人信息

--- src/main/java/com/chiyun/outboundplatform/repository/CasepeoplemessageRepository.java
+++ src/main/java/com/chiyun/outboundplatform/repository/CasepeoplemessageRepository.java
@@ -4,6 +4,9 @@ import com.chiyun.outboundplatform.entity.CasepeoplemessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by wazto on 2019/3/21.
  */
@@ -11,4 +14,10 @@ public interface CasepeoplemessageRepository extends JpaRepository<Casepeoplemes
 
     @Query(value = "update casepeoplemessage set show_state = 1 where case_id = ?1", nativeQuery = true)
     void setXszt(Integer ajid);
+
+    /**
+     *  查询业务员接单之后的根据任务状态查询的所有任务的任务id
+     */
+    @Query(value = "select * from casepeoplemessage where case_id = ?1", nativeQuery = true)
+    CasepeoplemessageEntity findByAjid(Integer ajid);
 }
--- src/main/java/com/chiyun/outboundplatform/repository/TaskRepository.java
+++ src/main/java/com/chiyun/outboundplatform/repository/TaskRepository.java
@@ -76,11 +76,6 @@ public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {
      */
     Page<TaskEntity> findAllByRwzxrAndRwzt(Integer ywyid, Integer rwzt, Pageable pageable);
 
-    /**
-     *  查询业务员接单之后的根据任务状态查询的所有任务的任务id
-     */
-    @Query(value = "select * from casepeoplemessage where case_id in (select distinct case_id from task where task_people = ?1 and task_state = ?2) order by case_id desc ", nativeQuery = true)
-    List<Map<String, Object>> findAllByAjids(Integer ywyid, Integer rwzt);
 
     /**
      * 多条件查询:任务名称、任务截止时间、任务方式、任务状态、审核状态、任务执行人、任务完成时间

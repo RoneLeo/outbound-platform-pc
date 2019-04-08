bec40cf6f9ebf0ff9b018c893a9248e8fe823061
qiu <2411929973@qq.com>
2019-04-08 15:28:29 +0800

    添加用户时设置用户总金额

--- src/main/java/com/chiyun/outboundplatform/web/UserController.java
+++ src/main/java/com/chiyun/outboundplatform/web/UserController.java
@@ -27,6 +27,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.math.BigDecimal;
 import java.util.*;
 
 
@@ -183,6 +184,7 @@ public class UserController {
         userEntity.setCjsj(new Date());
         userEntity.setZt(0);
         userEntity.setMm(MD5Util.MD5("666666"));
+        userEntity.setZje(new BigDecimal("0"));
         UserEntity userEntity1 = userReposity.save(userEntity);
         String sqm = CodeUtil.toSerialCode(userEntity1.getId());
         userEntity1.setSqm(sqm);
